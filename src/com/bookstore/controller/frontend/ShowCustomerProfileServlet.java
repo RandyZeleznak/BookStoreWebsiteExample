2970ce9ed2a59700aa57f1e49891ffd3f145124e
Randy <RandyZeleznak@gmail.com>
2020-03-27 18:37:10 -0500

    Section#125 completion - 03/27/20:1800

---
+++ src/com/bookstore/controller/frontend/ShowCustomerProfileServlet.java
@@ -0,0 +1,30 @@
+package com.bookstore.controller.frontend;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bookstore.service.CustomerServices;
+
+/**
+ * Servlet to show the Customer Profile
+ */
+@WebServlet("/view_profile")
+public class ShowCustomerProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ShowCustomerProfileServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CustomerServices customerServices = new CustomerServices(request, response);
+		customerServices.showCustomerProfile();
+	}
+
+}
